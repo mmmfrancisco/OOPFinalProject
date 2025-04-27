c2a7e534a3e723cada249a699dc0736c1f6ff927
Francisco, Maica <fjvmanulat@antiquespride.com>
2025-04-27 23:40:27 +0800

    Onging update.

--- Finals-Project-Group5/Finals-Project-Group5/Models/Book.cs
+++ Finals-Project-Group5/Finals-Project-Group5/Models/Book.cs
@@ -1,6 +1,19 @@
-﻿namespace Finals_Project_Group5.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Finals_Project_Group5.Models;
+
+namespace Finals_Project_Group5.Models
 {
     public class Book
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public string Description { get; set; }
     }
 }
