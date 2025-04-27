a50379297c7d7724a6beeae19477471fcf8a79f8
Maica Merylle <mmmfrancisco@antiquespride.edu.ph>
2025-04-27 23:14:04 +0800

    Add files via upload

---
+++ Finals-Project-Group5/Finals-Project-Group5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Finals_Project_Group5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
