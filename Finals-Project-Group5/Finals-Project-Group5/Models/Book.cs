220576d1d39ae03e61e2e5813276c31ebf18ba96
Francisco, Maica <fjvmanulat@antiquespride.com>
2025-04-28 20:09:49 +0800

    We've added a Book program.

---
+++ Finals-Project-Group5/Finals-Project-Group5/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPFinalsProject.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }  // Primary Key
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
