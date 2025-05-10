f4ec55efba59f0f03ab7a1d4cd262dc69879ddf9
Francisco, Maica <mmmfrancisco@antiquespride.com>
2025-05-10 23:10:46 +0800

    Our Person model includes properties Id, Name, and Age.

---
+++ Finals-Project-Group5/Finals-Project-Group5/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Finals_Project_Group5.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
+
