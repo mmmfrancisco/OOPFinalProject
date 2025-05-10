2f09680edddd157b95531418d94b4948ce381b3c
Francisco, Maica <mmmfrancisco@antiquespride.com>
2025-05-10 23:05:10 +0800

    The PersonsController handles creating part.

---
+++ Finals-Project-Group5/Finals-Project-Group5/Controllers/PersonsController.cs
@@ -0,0 +1,40 @@
+﻿using Finals_Project_Group5.Data;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Finals_Project_Group5.Models;
+
+namespace Finals_Project_Group5.Controllers
+{
+    public class PersonsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Persons.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+    }
+}
