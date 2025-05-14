ac6517c496fe80c240f973010912c954e7c6317d
Francisco, Maica <mmmfrancisco@antiquespride.com>
2025-05-14 22:06:52 +0800

    Data Folder, manage database connections and data context. This also connects to the database.

--- Finals-Project-Group5/Finals-Project-Group5/Data/ApplicationDbContext.cs
+++ Finals-Project-Group5/Finals-Project-Group5/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System;
 using Microsoft.EntityFrameworkCore;
+using Finals_Project_Group5.Models;  // Ensure this is included
 
 namespace Finals_Project_Group5.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Person> Persons { get; set; }  // Ensure this is correct
     }
 }
