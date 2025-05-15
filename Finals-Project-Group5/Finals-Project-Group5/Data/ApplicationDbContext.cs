acba5cbfc20c54625a94a4b8098aa987d57cc5b1
Francisco, Maica <mmmfrancisco@antiquespride.com>
2025-05-15 22:55:38 +0800

    Updated, for login authentication.

--- Finals-Project-Group5/Finals-Project-Group5/Data/ApplicationDbContext.cs
+++ Finals-Project-Group5/Finals-Project-Group5/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
-using Finals_Project_Group5.Models;  // Ensure this is included
+using Finals_Project_Group5.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;  // Ensure this is included
 
 namespace Finals_Project_Group5.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
